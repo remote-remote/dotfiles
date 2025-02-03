b7c8be54c48caf03f1f454ab17f98705fcf44572
remote-remote <188308486+remote-remote@users.noreply.github.com>
2025-02-03 09:53:47 -0600

    c-sharp

--- nvim/.config/nvim/lua/plugins/lsp/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp/lsp-config.lua
@@ -107,6 +107,11 @@ return {
       on_attach = on_attach,
     })
 
+    lspconfig["csharp_ls"].setup({
+      capabilities = capabilities,
+      on_attach = on_attach,
+    })
+
     lspconfig["lua_ls"].setup({
       capabilities = capabilities,
       on_attach = on_attach,
