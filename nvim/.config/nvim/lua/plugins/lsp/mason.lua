81f170745c35edbf10bade22ecf7342dfacab56d
remote-remote <188308486+remote-remote@users.noreply.github.com>
2025-06-30 13:15:12 -0500

    update mason source

--- nvim/.config/nvim/lua/plugins/lsp/mason.lua
+++ nvim/.config/nvim/lua/plugins/lsp/mason.lua
@@ -1,15 +1,15 @@
 return {
 	{
-		"williamboman/mason.nvim",
+		"mason-org/mason.nvim",
 		config = function()
 			local mason = require("mason")
 			mason.setup({})
 		end,
 	},
 	{
-		"williamboman/mason-lspconfig.nvim",
+		"mason-org/mason-lspconfig.nvim",
 		dependencies = {
-			"williamboman/mason.nvim",
+			"mason-org/mason.nvim",
 		},
 		config = function()
 			local mason_lspconfig = require("mason-lspconfig")
